4722bbdb154be2e8fdea2825f0958c909415e943
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2018-12-11 20:44:42 +0100

    added two tests

--- implementation/tfdacmacs/centralServer/src/test/java/de/tuberlin/tfdacmacs/centralserver/user/UserIntegrationTest.java
+++ implementation/tfdacmacs/centralServer/src/test/java/de/tuberlin/tfdacmacs/centralserver/user/UserIntegrationTest.java
@@ -1,6 +1,7 @@
 package de.tuberlin.tfdacmacs.centralserver.user;
 
 import de.tuberlin.tfdacmacs.IntegrationTestSuite;
+import de.tuberlin.tfdacmacs.centralserver.user.data.dto.UserCreationRequest;
 import de.tuberlin.tfdacmacs.centralserver.user.data.dto.UserCreationResponse;
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
@@ -14,19 +15,15 @@ public class UserIntegrationTest extends IntegrationTestSuite {
 
     @Test
     public void createUser() {
-        // TODO: fix me
+        UserCreationRequest userCreationRequest = new UserCreationRequest("test@tu-berlin.de");
+
         ResponseEntity<UserCreationResponse> userCreationResponseResponseEntity = restTemplate
-                .exchange("/users", HttpMethod.POST, HttpEntity.EMPTY, UserCreationResponse.class);
+                .exchange("/users", HttpMethod.POST, new HttpEntity(userCreationRequest), UserCreationResponse.class);
         assertThat(userCreationResponseResponseEntity.getStatusCode()).isEqualByComparingTo(HttpStatus.CREATED);
         UserCreationResponse body = userCreationResponseResponseEntity.getBody();
         String id = body.getId();
-        assertThat(id).isNotBlank();
+        assertThat(id).isNotBlank().isEqualTo("test@tu-berlin.de");
         assertThat(cryptEngine.isSignatureAuthentic(body.getIdSignature(), id, cryptEngine.getPublicKey())).isTrue();
         assertThat(userDB.exist(id)).isTrue();
-    }
-
-    @Test
-    public void getUser() {
-        // TODO: implement me
     }
 }
