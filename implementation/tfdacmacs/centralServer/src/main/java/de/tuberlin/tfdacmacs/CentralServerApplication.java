1f0dfce1b2f542643f996a545c73ddd2860678c2
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2018-12-26 17:43:20 +0100

    added swagger

--- implementation/tfdacmacs/centralServer/src/main/java/de/tuberlin/tfdacmacs/CentralServerApplication.java
+++ implementation/tfdacmacs/centralServer/src/main/java/de/tuberlin/tfdacmacs/CentralServerApplication.java
@@ -3,9 +3,11 @@ package de.tuberlin.tfdacmacs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
 public class CentralServerApplication {
 
     public static void main(String[] args) {
