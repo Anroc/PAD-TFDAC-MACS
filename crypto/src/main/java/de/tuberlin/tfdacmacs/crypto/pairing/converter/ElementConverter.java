44b5ae622c1492f4cbbcb35cd1725a7b0f5154bf
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2019-01-15 20:19:30 +0100

    implemented authority rest test

--- crypto/src/main/java/de/tuberlin/tfdacmacs/crypto/pairing/converter/ElementConverter.java
+++ crypto/src/main/java/de/tuberlin/tfdacmacs/crypto/pairing/converter/ElementConverter.java
@@ -2,28 +2,23 @@ package de.tuberlin.tfdacmacs.crypto.pairing.converter;
 
 import it.unisa.dia.gas.jpbc.Element;
 import it.unisa.dia.gas.jpbc.Field;
+import lombok.NonNull;
 
 import java.util.Base64;
 
 public class ElementConverter {
 
-    public static String convert(Element element) {
-        if(element == null) {
-            return null;
-        }
+    public static String convert(@NonNull Element element) {
         return new String(
                 Base64.getEncoder().encode(element.toBytes())
         );
     }
 
-    public static Element convert(String base64, Field field) {
-        if(base64 == null) {
-            return null;
-        }
+    public static Element convert(@NonNull String base64, @NonNull Field field) {
         return convert(Base64.getDecoder().decode(base64), field);
     }
 
-    public static Element convert(byte[] bytes, Field field) {
+    public static Element convert(byte[] bytes, @NonNull Field field) {
         return field.newElementFromBytes(bytes);
     }
 }
