2d8aa422e1866f1d40f84eed73c5e93c8814e9ff
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2019-02-03 17:35:15 +0100

    strange things are happen here

--- implementation/tfdacmacs/client/src/main/java/de/tuberlin/tfdacmacs/client/rest/factory/AAClientFactory.java
+++ implementation/tfdacmacs/client/src/main/java/de/tuberlin/tfdacmacs/client/rest/factory/AAClientFactory.java
@@ -1,6 +1,7 @@
 package de.tuberlin.tfdacmacs.client.rest.factory;
 
 import de.tuberlin.tfdacmacs.client.authority.events.TrustedAuthorityUpdatedEvent;
+import de.tuberlin.tfdacmacs.client.config.ClientConfig;
 import de.tuberlin.tfdacmacs.client.register.Session;
 import de.tuberlin.tfdacmacs.client.rest.template.AAClientTemplate;
 import de.tuberlin.tfdacmacs.client.rest.template.RestTemplateFactory;
@@ -21,14 +22,23 @@ public class AAClientFactory {
     private final RestTemplateFactory restTemplateFactory;
     private final Session session;
 
+    private final ClientConfig clientConfig;
+
     @EventListener(TrustedAuthorityUpdatedEvent.class)
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public void createAAClient(TrustedAuthorityUpdatedEvent trustedAuthorityUpdatedEvent) {
         String aid = trustedAuthorityUpdatedEvent.getSource().getId();
         BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(AAClientTemplate.class);
 
+        String url = aid;
+
+        // TODO: just for testing, remove later
+        if(aid.equals("aa.tu-berlin.de")) {
+            url = clientConfig.getAaRootUrl();
+        }
+
         RestTemplate restTemplate = restTemplateFactory
-                .buildRestTemplate(aid, null);
+                .buildRestTemplate(url, null);
 
         if(session.isActive()) {
             restTemplateFactory.updateRestTemplate(session.getEmail(), restTemplate);
