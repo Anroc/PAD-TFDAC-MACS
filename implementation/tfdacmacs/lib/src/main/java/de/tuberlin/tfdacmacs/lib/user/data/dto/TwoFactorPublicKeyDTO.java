94e1ab285b44a5c76a8e3360fce8585dd007becf
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2019-02-17 16:27:19 +0100

    save

---
+++ implementation/tfdacmacs/lib/src/main/java/de/tuberlin/tfdacmacs/lib/user/data/dto/TwoFactorPublicKeyDTO.java
@@ -0,0 +1,26 @@
+package de.tuberlin.tfdacmacs.lib.user.data.dto;
+
+import de.tuberlin.tfdacmacs.crypto.pairing.data.Versioned;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TwoFactorPublicKeyDTO implements Versioned {
+
+    @Min(0)
+    private int version;
+
+    @NotBlank
+    private String twoFactorAuthenticationPublicKey;
+
+    @NotBlank
+    // userId + twoFactorAuthencationPublicKey + version
+    private String signature;
+
+}
