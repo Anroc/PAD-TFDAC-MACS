c657f205fcea3637337a2fb19234e79dbfbc465c
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2019-03-12 11:44:10 +0100

    added no args constructor

--- crypto/src/main/java/de/tuberlin/tfdacmacs/crypto/pairing/data/keys/TwoFactorUpdateKey.java
+++ crypto/src/main/java/de/tuberlin/tfdacmacs/crypto/pairing/data/keys/TwoFactorUpdateKey.java
@@ -1,8 +1,12 @@
 package de.tuberlin.tfdacmacs.crypto.pairing.data.keys;
 
 import it.unisa.dia.gas.jpbc.Element;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+@Data
+@NoArgsConstructor
 public class TwoFactorUpdateKey extends UserUpdateKey {
     public TwoFactorUpdateKey(@NonNull String userId, @NonNull Element updateKey, long targetVersion) {
         super(userId, updateKey, targetVersion);
