9193fe447c2ee60530dbfa07f0e2a5d1198d9815
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2019-03-16 15:47:14 +0100

    more bug fixes

--- implementation/tfdacmacs/crypto/src/main/java/de/tuberlin/tfdacmacs/crypto/pairing/ABEEncryptor.java
+++ implementation/tfdacmacs/crypto/src/main/java/de/tuberlin/tfdacmacs/crypto/pairing/ABEEncryptor.java
@@ -94,7 +94,7 @@ public class ABEEncryptor extends ABECrypto {
             }
         }
 
-        if(cipherText.isTwoFactorSecured() && cipherText.getOwnerId().equals(cipherTextAttributeUpdateKey.getDataOwnerId())) {
+        if(cipherText.isTwoFactorSecured() && ! cipherText.getOwnerId().equals(cipherTextAttributeUpdateKey.getDataOwnerId())) {
             throw new VersionMismatchException(
                     String.format("Given cipher text 2FA key has version %s but update key version was %s", cipherText.getOwnerId(), cipherTextAttributeUpdateKey.getDataOwnerId())
             );
