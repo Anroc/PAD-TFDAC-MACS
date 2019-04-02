5a687d355d426b443e328544692295d3e1c57ed8
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2019-04-02 20:05:15 +0200

    added updated graphics

---
+++ thesis/img/copy_benchmark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ../../implementation/benchmark/server-artifacts/and-policies/*.png eval-and-policy/
+cp ../../implementation/benchmark/server-artifacts/or-policies/*.png eval-or-policy/
+cp ../../implementation/benchmark/server-artifacts/join/*.png eval-join/
+cp ../../implementation/benchmark/server-artifacts/leave/*.png eval-leave/
+
